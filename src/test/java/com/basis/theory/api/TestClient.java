9fac5d4bf45ad783b1837d4140ed26c1b2d61150
Robert G. Greathouse <robert@basistheory.com>
2024-10-07 12:09:02 -0500

    Disable tests until setup can be completed

--- src/test/java/com/basis/theory/api/TestClient.java
+++ src/test/java/com/basis/theory/api/TestClient.java
@@ -32,6 +32,7 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled("Need to setup Production test tenant and setup Github Action")
 public final class TestClient {
     @Test
     public void shouldCallTenantSelf() {
